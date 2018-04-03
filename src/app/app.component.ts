b1db10049e29ea00c2bd08e1cb770c5fee0d69fb
Vijayakumari <vijaya@olivetech.net>
2018-04-03 18:25:15 +0530

    update the service worker

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
+import { OnInit } from '@angular/core/src/metadata/lifecycle_hooks';
+import { SwUpdate } from '@angular/service-worker';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+
+  constructor(private swUpdate: SwUpdate){
+
+  }
+
+  ngOnInit(){
+
+    if (this.swUpdate.isEnabled) {
+                console.log('am in update');
+                  this.swUpdate.available.subscribe(() => {
+      
+                      if(confirm("New version available. Load New Version?")) {
+      
+                          window.location.reload();
+                      }
+                  });
+              }
+  }
 }
